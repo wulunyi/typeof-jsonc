14b1f3d0f490c41de942510fec89e88f09dda321
wulunyi <wulunyi@meituan.com>
2019-03-17 22:32:56 +0800

    master 修复发布问题

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,5 +1,7 @@
 import * as dtsDom from 'dts-dom';
 import { ParseOptions } from 'jsonc-parser';
 import parser from './jsoncParser';
-declare function typeofJsonc(jsonc: string, name?: string, options?: Partial<dtsDom.EmitOptions & ParseOptions>): string;
+declare function typeofJsonc(jsonc: string, name?: string, options?: Partial<dtsDom.EmitOptions & ParseOptions & {
+    export: boolean;
+}>): string;
 export { parser, typeofJsonc };
