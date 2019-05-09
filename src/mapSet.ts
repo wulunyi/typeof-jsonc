1a99834c973ffc7c15b50ab4896432927a0f5007
wulunyi <wulunyi@meituan.com>
2019-05-09 10:11:41 +0800

    feat(feature/v1.1.8): 调整写法

--- src/mapSet.ts
+++ src/mapSet.ts
@@ -2,11 +2,13 @@ export default class MapSet<T = any> {
     private $map: Map<any, Set<T>> = new Map();
 
     public add(key: any, value: T) {
-        if (!this.$map.get(key)) {
-            this.$map.set(key, new Set());
-        }
+        const cacheSet = this.$map.get(key);
 
-        this.$map.get(key)!.add(value);
+        if (!cacheSet) {
+            this.$map.set(key, new Set());
+        } else {
+            cacheSet.add(value);
+        }
     }
 
     public get(key: any): T[] {
